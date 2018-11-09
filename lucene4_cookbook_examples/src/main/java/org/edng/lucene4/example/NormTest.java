10f81107ba07c37a0de5e47c958545c6e2408a7d
Maksym-dev <magridin1995@gmail.com>
2018-11-09 12:47:18 +0200

    Added Lucene 4 examples

---
+++ lucene4_cookbook_examples/src/main/java/org/edng/lucene4/example/NormTest.java
@@ -0,0 +1,66 @@
+package org.edng.lucene4.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+/**
+ * Created by ed on 12/24/14.
+ */
+public class NormTest {
+
+    @org.junit.Test
+    public void runTest() throws Exception {
+
+        Analyzer analyzer = new StandardAnalyzer();
+        Directory directory = new RAMDirectory();
+        IndexWriterConfig config = new IndexWriterConfig(Version.LATEST, analyzer);
+        IndexWriter indexWriter = new IndexWriter(directory, config);
+
+        Document doc = new Document();
+        TextField textField = new TextField("name", "", Field.Store.YES);
+
+        float boost = 1f;
+        String[] names = {"John R Smith", "Mary Smith", "Peter Smith"};
+        for (String name : names) {
+            boost *= 1.1;
+            textField.setStringValue(name);
+            //textField.setBoost(boost);
+            doc.removeField("name");
+            doc.add(textField);
+            indexWriter.addDocument(doc);
+        }
+
+        indexWriter.commit();
+
+        IndexReader indexReader = DirectoryReader.open(directory);
+        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+        Query query = new TermQuery(new Term("name", "smith"));
+        TopDocs topDocs = indexSearcher.search(query, 100);
+        System.out.println("Searching 'smith'");
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            doc = indexReader.document(scoreDoc.doc);
+            if (scoreDoc.equals(topDocs.scoreDocs[0])) {
+                assertEquals("Rank 1 not match", "Mary Smith", doc.getField("name").stringValue());
+            }
+            System.out.println(doc.getField("name").stringValue());
+        }
+    }
+}
