49ba302f3703d37f3dff797e99f653bef6916a30
Maksym-dev <magridin1995@gmail.com>
2018-11-12 20:21:20 +0200

    Optimized WordNetSynonymsTest

--- lucene3_cookbook_examples/src/test/java/org/edng/lucene3/example/WordNetSynonymsTest.java
+++ lucene3_cookbook_examples/src/test/java/org/edng/lucene3/example/WordNetSynonymsTest.java
@@ -8,23 +8,33 @@ import org.apache.lucene.util.Version;
 import org.apache.lucene.wordnet.SynExpand;
 import org.apache.lucene.wordnet.SynLookup;
 import org.apache.lucene.wordnet.Syns2Index;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 
 public class WordNetSynonymsTest {
 
+    private static final String INDEX_DIR = "data/index330";
+
+    @BeforeClass
+    public static void setUpIndex() throws Throwable {
+        File file = new File(INDEX_DIR);
+        if (!file.exists()) {
+            String[] args = {"wn_s.pl", INDEX_DIR};
+            Syns2Index.main(args);
+        }
+    }
+
     @Test
     public void testExpand() throws Throwable {
-        String indexDir = "data/index777";
-        String[] args2 = {indexDir, "big dog"};
+        String[] args2 = {INDEX_DIR, "big dog"};
         SynExpand.main(args2);
     }
 
     @Test
     public void testExpandExtended() throws Throwable {
-        String indexDir = "data/index777";
-        String[] args2 = {indexDir, "big d?g"};
+        String[] args2 = {INDEX_DIR, "big d?g"};
 
         FSDirectory directory = FSDirectory.open(new File(args2[0]));
         IndexSearcher searcher = new IndexSearcher(directory, true);
@@ -33,20 +43,12 @@ public class WordNetSynonymsTest {
 
         Query q = SynExpandExt.expand( query, searcher, new StandardAnalyzer(Version.LUCENE_CURRENT), field, 1.2f,0.9f);
         System.out.println( "Query: " + q.toString( field));
-//        SynExpand.main(args2);
     }
 
     @Test
     public void synLoockup() throws Throwable {
-        String indexDir = "data/index777";
-        String[] args2 = {indexDir, "big"};
+        String[] args2 = {INDEX_DIR, "big"};
         SynLookup.main(args2);
     }
 
-    @Test
-    public void indexSynonyms() throws Throwable {
-        String indexDir = "data/index777";
-        String[] args = {"wn_s.pl", indexDir};
-        Syns2Index.main(args);
-    }
 }
